9c08c89266734ffb80b771fde6d63c844005c587
y2kyg0r <ygor.passosluciano129@gmail.com>
2025-04-02 21:15:20 -0300

    Lançado o MVP do projeto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { FaGithubSquare } from "react-icons/fa";
+
+function App() {
+ 
+
+  return (
+    <div className='container-app'>
+      <h1>Meus Links</h1>
+
+      <a href="https://github.com/y2kyg0r" className='links' target='_blanck'><FaGithubSquare />Github</a>
+      
+    </div>
+  )
+}
+
+export default App
